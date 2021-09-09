66b049d50bfa0278e627cdc901c34befc4ec85e2
BaimingZhu <573514187@qq.com>
2021-09-09 12:56:01 +0800

    todoList

--- react-staging/02_src_hello_react/components/Hello/index.jsx
+++ react-staging/02_src_hello_react/components/Hello/index.jsx
@@ -1,5 +1,5 @@
 import React,{Component} from 'react'
-import hello from './index.module.css'
+import hello from './index.module.css'//避免相同类名会覆盖的问题
 
 export default class Hello extends Component{
     render(){
--- react-staging/src/APP.jsx
+++ react-staging/src/APP.jsx
@@ -1,10 +1,37 @@
 import React, { Component } from 'react'
+import Header from './components/Header'
+import List from './components/List'
+import Footer from './components/Footer'
+import './App.css'
 
 export default class APP extends Component {
+
+    //初始化状态
+    state = {todos:[
+        {id:'001', name:'吃饭', done:true},
+        {id:'002', name:'睡觉', done:false},
+        {id:'003', name:'打代码', done:false},
+    ]}
+
+    //用于添加一个todo，接受的参数时todo对象
+    addTodo = (todoObj) => {
+        // 获取原todos
+        const {todos} = this.state
+        //追加一个todo
+        const newTodos = [todoObj, ...todos]
+        //更新状态
+        this.setState({todos : newTodos})
+    }
+
     render() {
+        const {todos} = this.state
         return (
-            <div>
-                <h2>App....</h2>
+            <div className='todo-container'>
+               <div className='todo-wrap'>
+                    <Header addTodo={this.addTodo}/>
+                    <List todos={todos}/>
+                    <Footer/>
+               </div>
             </div>
         )
     }
