b164b2e0dfd002ff89efc55c05d8fce1122499d0
BaimingZhu <573514187@qq.com>
2021-09-12 23:02:08 +0800

    todoLIst

--- react-staging/src/components/Item/index.jsx
+++ react-staging/src/components/Item/index.jsx
@@ -2,15 +2,36 @@ import React, { Component } from 'react'
 import './index.css'
 
 export default class Item extends Component {
+
+    state = {mouse: false}
+
+    //处理勾选与未勾选的情况
+    handelCheck = (id) => {
+        return (event) => {
+            // console.log(id, event.target.checked)
+            const {checked} = event.target
+            this.props.updateTodo(id, checked)
+        }
+    }
+
+
+    //标识鼠标移入移出的回调
+    handelMouse = (flag) => {
+        return () => {
+            this.setState({mouse: flag})
+        }
+    } 
+
     render() {
         const {name, id, done} = this.props
+        const {mouse} = this.state
         return (
-            <li>
+            <li style={{backgroundColor: mouse ? "#ddd" : 'white'}} onMouseEnter={this.handelMouse(true)} onMouseLeave={this.handelMouse(false)}>
             <label>
-                <input type="checkbox" defaultChecked={done}/>
+                <input type="checkbox" defaultChecked={done} onChange={this.handelCheck(id)}/>
                 <span>{name}</span>
             </label>
-            <button className="btn btn-danger" style={{display:'none'}}>删除</button>
+            <button className="btn btn-danger" style={{display: mouse ? 'block' : 'none'}}>删除</button>
         </li>
         )
     }
