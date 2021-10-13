986f0f9861772bbcd0e82a9cc4879e33dd7d349a
BaimingZhu <573514187@qq.com>
2021-10-13 14:27:50 +0800

    脚手架配置代理

--- react-staging/src/APP.jsx
+++ react-staging/src/APP.jsx
@@ -4,7 +4,16 @@ import axios from 'axios'
 export default class APP extends Component {
 
     getStudentData = () => {
-        axios.get('http://localhost:3000/students').then(
+        axios.get('http://localhost:3000/api1/students').then(
+            response=> {
+                console.log('成功了', response.data)
+            },
+            error => { console.log('失败了', error)}
+        )
+    }
+
+    getCarData = () => {
+        axios.get('http://localhost:3000/api2/cars').then(
             response=> {
                 console.log('成功了', response.data)
             },
@@ -16,6 +25,7 @@ export default class APP extends Component {
         return (
             <div>
                <button onClick={this.getStudentData} >点我获取学生数据</button>
+               <button onClick={this.getCarData} >点我获汽车数据</button>
             </div>
         )
     }
