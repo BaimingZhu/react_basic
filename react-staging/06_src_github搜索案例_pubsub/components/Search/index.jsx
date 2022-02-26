3837dddcb0e0c7c47e869119cd25a9874465878c
BaimingZhu <573514187@qq.com>
2022-02-26 21:31:32 +0800

    07_src_github搜索案例_fetch

--- react-staging/06_src_github搜索案例_pubsub/components/Search/index.jsx
+++ react-staging/06_src_github搜索案例_pubsub/components/Search/index.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import PubSub  from 'pubsub-js'
-// import axios   from 'axios'
+import axios   from 'axios'
 
 export default class Search extends Component {
 
 
-    search = async() => {
+    search = () => {
         //获取用户输入(连续解构赋值+重命名)
         const {keyWordElement : {value: keyword} } = this
         console.log(keyword);
@@ -14,9 +14,8 @@ export default class Search extends Component {
         // this.props.updateAppState({isFirst:false, isLoading:true})
         PubSub.publish('changeState', {isFirst:false, isLoading:true})
         
-        //#region 发送网络请求
-        /*
-        axios.get(`http://localhost:3000/api1/search/users2?q=${keyword}`).then(
+        //发送网络请求
+        axios.get(`https://api.github.com/search/users?q=${keyword}`).then(
             response => {
                 console.log('成功了', response.data)
                 //请求成功后通知List更新状态
@@ -28,39 +27,7 @@ export default class Search extends Component {
                 // this.props.updateAppState({isLoading:false, err:error.message})
                 PubSub.publish('changeState', {isLoading:false, err:error.message})
             }
-        )*/
-        //#endregion
-
-        // 发送网络请求
-        // fetch(`/api1/search/users2?q=${keyword}`).then(
-        //     response => { 
-        //         console.log('联系服务器成功了')
-        //         return response.json()
-        //     },
-        //     error => { 
-        //         console.log('联系服务器失败了', error)
-        //     }
-        // ).then(
-        //     response => { 
-        //         console.log('成功了', response)
-        //         PubSub.publish('changeState', {isLoading:false, users: response.items, err:''})
-        //     },
-        //     error => { 
-        //         console.log('失败了', error)
-        //         PubSub.publish('changeState', {isLoading:false, err:error.message})
-        //     }
-        // )
-            
-        //发送网络请求优化
-        try {
-            const response= await fetch(`/api1/search/users2?q=${keyword}`)
-			const data = await response.json()
-            console.log("data", data)
-            PubSub.publish('changeState', {isLoading:false, users: data.items, err:''})
-        } catch (error) {
-            console.log('请求错误', error)
-            PubSub.publish('changeState', {isLoading:false, err:error.message})
-        }
+        )
     }
 
     render() {
