7baa56e7c111d625c378f96c59ece644964a7df3
BaimingZhu <573514187@qq.com>
2022-03-09 22:32:03 +0800

    t

--- react-staging/src/APP.jsx
+++ react-staging/src/APP.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {Route, Switch} from 'react-router-dom'
+import {Route, Switch, Redirect} from 'react-router-dom'
 import Home from './pages/Home'//路由组件
 import About from './pages/About'
 import Header from './components/Header'//一般组件
@@ -22,11 +22,8 @@ export default class App extends Component {
 							{/* <a className="list-group-item" href="./about.html">About</a>
 							<a className="list-group-item active" href="./home.html">Home</a> */}
 
-							{/* 在React中靠路由链接实现切换组件--编写路由链接 */}
-							{/* <NavLink activeClassName="zhubaiming" className="list-group-item" to="/about">About</NavLink>
-							<NavLink activeClassName="zhubaiming" className="list-group-item" to="/home">Home</NavLink> */}
-							<MyNavLink to="/zhubaiming/about">About</MyNavLink>
-							<MyNavLink to="/zhubaiming/home">Home</MyNavLink>
+							<MyNavLink to="/about">About</MyNavLink>
+							<MyNavLink to="/home">Home</MyNavLink>
 						</div>
 					</div>
 					<div className="col-xs-6">
@@ -34,8 +31,9 @@ export default class App extends Component {
 							<div className="panel-body">
 								{/* 注册路由 */}
 								<Switch>
-									<Route path="/zhubaiming/about" component={About}/>
-									<Route path="/zhubaiming/home" component={Home}/>
+									<Route path="/about" component={About}/>
+									<Route path="/home" component={Home}/>
+									<Redirect to="/about"></Redirect>
 								</Switch>
 							</div>
 						</div>
