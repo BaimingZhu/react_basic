897d833415adc013a3c39b54ac0dcc2e03ce627d
BaimingZhu <573514187@qq.com>
2022-04-18 13:03:00 +0800

    6_src_react-redux优化

--- redux_test/6_src_react-redux优化/container/Count/index.jsx
+++ redux_test/6_src_react-redux优化/container/Count/index.jsx
@@ -1,5 +1,4 @@
-//引入Count的UI组件
-import CountUI from '../../components/Count'
+import React, { Component } from 'react'
 //引入action
 import {
 	createIncrementAction,
@@ -10,22 +9,59 @@ import {
 //引入connect用于连接UI组件与redux
 import {connect} from 'react-redux'
 
+class Count extends Component {
+    state = { count : 0 }
+
+    //加法
+    increment = () => {
+        const {value} = this.selectNumber
+        this.props.jia(value * 1)
+    }
+    //减法
+    decrement = () => {
+        const {value} = this.selectNumber
+        this.props.jian(value * 1)
+    }
+    //奇数再加
+    incrementIfOdd = () => {
+        const {value} = this.selectNumber
+        if(this.props.count % 2 !== 0){
+            this.props.jia(value * 1)
+        }
+    }
+    //异步加
+    incrementAsync = () => {
+        const {value} = this.selectNumber
+        this.props.jiaAsync(value*1,500) 
+    }
+
+  render() {
+    return (
+      <div>
+          <h1>当前求和为：{this.props.count}</h1>
+          <select name="number" ref={c => this.selectNumber = c}>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+          </select>&nbsp;
+          <button onClick={this.increment}>+</button>&nbsp;
+          <button onClick={this.decrement}>-</button>&nbsp;
+          <button onClick={this.incrementIfOdd}>当前求和为奇数再加</button>&nbsp;
+          <button onClick={this.incrementAsync}>异步加</button>&nbsp;
+      </div>
+    )
+  }
+}
+
 //使用connect()()创建并暴露一个Count的容器组件
 export default connect(
+	//映射UI组件的state
 	state => ({count:state}),
-
-	//mapDispatchToProps的一般写法
-	/* dispatch => ({
-		jia:number => dispatch(createIncrementAction(number)),
-		jian:number => dispatch(createDecrementAction(number)),
-		jiaAsync:(number,time) => dispatch(createIncrementAsyncAction(number,time)),
-	}) */
-
-	//mapDispatchToProps的简写
+	//映射UI组件props
 	{
 		jia  : createIncrementAction,
 		jian : createDecrementAction,
 		jiaAsync : createIncrementAsyncAction,
 	}
-	)(CountUI)
+	)(Count)
 
