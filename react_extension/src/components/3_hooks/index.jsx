932f786ad14cda11bf780ea34ffd34f31a1a751e
BaimingZhu <573514187@qq.com>
2022-05-22 13:47:44 +0800

    Ref Hook

--- react_extension/src/components/3_hooks/index.jsx
+++ react_extension/src/components/3_hooks/index.jsx
@@ -3,6 +3,13 @@ import React from 'react'
 // export default class Count extends React.Component {
 
 //     state = { count : 0}
+
+//     myRef = React.createRef()
+
+//     show = () => {
+// 		alert(this.myRef.current.value)
+// 	}
+
 //     addCount = () => {
 //         this.setState( state => ({count : state.count + 1}))
 //     }
@@ -23,9 +30,11 @@ import React from 'react'
 //     render() {
 //         return (
 //             <div>
+//                 <input type="text" ref={this.myRef} />
 //                 <h1>当前求和为：{this.state.count}</h1>
 //                 <button onClick={this.addCount}>点我+1</button>
 //                 <button onClick={this.unMount}>卸载组件</button>
+//                 <button onClick={this.show}>点击提示数据</button>
 //             </div>
 //         )
 //     }
@@ -37,6 +46,12 @@ function Count() {
     const [count, setCount] = React.useState(0)
     const addCount = () => {
         setCount(count + 1)
+    }
+
+    const myRef = React.useRef()
+
+    const show = () => {
+        alert(myRef.current.value)
     }
     
     React.useEffect( () => {
@@ -55,9 +70,11 @@ function Count() {
 
     return (
         <div>
+            <input type="text" ref={myRef} />
             <h1>当前求和为：{count}</h1>
             <button onClick={addCount}>点我+1</button>
             <button onClick={unmount}>卸载组件</button>
+            <button onClick={show}>点我展示数据</button>
         </div>
     )
 }
